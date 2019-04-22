3d1c12259446d5f394359799f45d1858d54f86e0
tactnician <tactnician@gmail.com>
2019-04-22 16:29:11 -0400

    updated index to list endpoints

--- index.js
+++ index.js
@@ -4,5 +4,19 @@ const server = require('./api/server.js');
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
-  console.log(`\n *** Server listening on port ${port}\n ***`);
+  console.log(`\n *** Server listening on port ${port} ***\n`);
 });
+
+server.get('/', (req, res) => {
+  res.send(`
+  <h1>Api Portal</h1>
+  <h3>Endpoint:</h3>
+  <p></p>
+  <p>~/api/login</p>
+  <p>~/api/register</p>
+  <p></p>
+  <h3>Methods </h3>
+  <p>POST {username: '', password ''} to '/api/register'</p>
+  <p>POST {username: '', password: '' } to '/api/login' </p>
+  `)
+})
