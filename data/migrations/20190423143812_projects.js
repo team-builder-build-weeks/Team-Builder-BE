a2ec1a2412011ac55f59b7b89509f856e3fef1d2
tactnician <tactnician@gmail.com>
2019-04-24 13:52:34 -0400

    fixed but with id in project migration

--- data/migrations/20190423143812_projects.js
+++ data/migrations/20190423143812_projects.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
     return knex.schema
         .createTable('projects', tbl => {
-            tbl.increments('_id');
+            tbl.increments('id');
 
             tbl
             .string('name', 128)
